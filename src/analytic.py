potential fix of a bug of principal orientation calculation

src/analytic.py
@@ -21,9 +21,15 @@ def analytic(a1, a2, w, ux0, n1, n2, es, e, dd):
     """
     d_w = (a2 - a1) / w
     ux0_w = ux0 / w
-    tmp = d_w + (1 - (1 - es / e) * (1 - 4 * n1**2 * n2**2)) * (1 - d_w)
-    s2 = ux0_w / tmp  # p_u1/p_y
-    s1 = (ux0_w - s2 * d_w) / (1 - d_w)
+    
+    denom = 1 - (1 - es / e) * (1 - 4 * n1**2 * n2**2) * (1 - d_w)
+    nom = 1 - (1 - es / e) * (1 - 4 * n1**2 * n2**2)
+
+    s1 = ux0_w * nom / denom
+    s2 = ux0_w / denom
+
+#    s2 = ux0_w / tmp  # p_u1/p_y
+#    s1 = (ux0_w - s2 * d_w) / (1 - d_w)
     
     d = np.arange(-w, 0 + dd, dd)  # Creating the depth array
     
@@ -72,8 +78,12 @@ def calc_principal(sxx, syy, sxy):
         nx0, ny0, nx1, ny1: Unit vectors for smax and smin orientations.
         J2: Second invariant of the stress tensor.
     """
-    t = np.degrees(np.arctan2(2 * sxy, (sxx - syy))) / 2
+    sig = np.array([
+    [sxx, sxy],
+    [sxy, syy]])
     
+    t = np.degrees(np.arctan2(2 * sxy, (sxx - syy))) / 2
+
     Q = np.array([[np.cos(np.radians(t)), np.sin(np.radians(t))],
                   [-np.sin(np.radians(t)), np.cos(np.radians(t))]])
     
@@ -89,24 +99,26 @@ def calc_principal(sxx, syy, sxy):
     if abs(princ_s[0, 1]) > tol:
         _ = princ_s[0, 1]
     
-    if sig1 <= sig2:  # Negative compressional
-        smax = sig1
-        smin = sig2
-        nx0 = np.cos(np.radians(t))
-        ny0 = -np.sin(np.radians(t))
-        nx1 = np.sin(np.radians(t))
-        ny1 = np.cos(np.radians(t))
-    else:
-        smax = sig2
-        smin = sig1
-        nx0 = np.sin(np.radians(t))
-        ny0 = np.cos(np.radians(t))
-        nx1 = np.cos(np.radians(t))
-        ny1 = -np.sin(np.radians(t))
+    smax = sig1
+    smin = sig2
+    nx0 = np.cos(np.radians(t))
+    ny0 = -np.sin(np.radians(t))
+    nx1 = np.sin(np.radians(t))
+    ny1 = np.cos(np.radians(t))
     
+    # using numpy linalg to compute eig values and vecs. 
+
+    eigvals, eigvecs = np.linalg.eigh(sig) 
+    sig1, sig2 = eigvals
+    dir1 = eigvecs[:,0]
+    dir2 = eigvecs[:,1] 
+
+    smax, smin = sig1, sig2 
+    dirmax, dirmin = dir1, dir2
+
     J2 = sxx * syy - sxy**2
     
-    return smax, smin, nx0, ny0, nx1, ny1, J2
+    return smax, smin, dirmax, dirmin, J2
 
 
 

src/run_analytic.py
@@ -21,16 +21,21 @@ id = round((mid_layer_depth-(-w))/dd+1) # counting from -w to 0, idx 8 is inside
 
 # compute
 d, sig11, sig12, sig22, str11, str12, str22, u1, p = analytic(a1, a2, w, ux0, n1, n2, es, e, dd)
-smax, smin, nx0, ny0, nx1, ny1, J2 = calc_principal(sig11[id-1], sig22[id-1], sig12[id-1])
-srmax, srmin, srnx0, srny0, srnx1, srny1, srJ2 = calc_principal(str11[id-1], str22[id-1], str12[id-1])
-srmax_iso, srmin_iso, srnx0_iso, srny0_iso, srnx1_iso, srny1_iso, srJ2_iso = calc_principal(str11[2], str22[2], str12[2])
-theta_sigma1 = np.arctan(ny0/nx0)/np.pi*180
-theta_eps1 = np.arctan(srny0/srnx0)/np.pi*180
 
-#print(smax, smin, nx0, ny0, nx1, ny1, J2)
-#print(srmax, srmin, srnx0, srny0, srnx1, srny1, srJ2)
-print(f"Normal director to weak anisotropy direction rotates from y+ at {theta:.2f} degs")
-print(f"The angle of sigma1 to x+ is {theta_sigma1:.2f}")
-print(f"The angle of eps1 to x+ is {theta_eps1:.2f}")
+smax, smin, smax_eigvec, smin_eigvec, s_J2 = calc_principal(sig11[id-1], sig22[id-1], sig12[id-1])
+srmax, srmin, srmax_eigvec, srmin_eigvec, srJ2 = calc_principal(str11[id-1], str22[id-1], str12[id-1])
+srmax_i, srmin_i, _, _, srJ2_iso = calc_principal(str11[2], str22[2], str12[2])
+
+theta_sigma_max = np.degrees(np.arctan(smax_eigvec[1]/smax_eigvec[0]))
+theta_eps_min = np.degrees(np.arctan(srmin_eigvec[1]/srmin_eigvec[0]))
+
+print(' ')
+print('********* results *********')
+print(f"1. Normal director to weak anisotropy direction rotates counterclockwisely from y+ at {theta:.2f} degs")
+print('2. Stress tensor [sxx, syy, sxy] = ', [sig11[id-1], sig22[id-1], sig12[id-1]])
+print(f"3. Principal stresses smax and smin are ", smax, smin)
+print(f"4. Rotate from the x axis clockwisely {-theta_sigma_max:.2f} to get the maximum compressive s_max")
+print(f"5. Principal strain rate eps_max and eps_min are ", srmax, srmin)
+print(f"6. Rotate from the x axis counterclockwisely {theta_eps_min:.2f} to get the minimum/extensional eps_min")
+
 print(f"Strain localization is {srJ2/srJ2_iso:.2f}")
-print(f"Pressure inside ani and iso layers are {p[id-1]:.2f} and {p[1]:.2f}")